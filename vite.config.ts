8a4683b703463b9da8d8e1bd88883b97e67f4f60
Parneet Kaur <parneetbuttar1153@gmail.com>
2026-02-09 18:38:53 -0700

    Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/parneet-portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,7 +11,5 @@ export default defineConfig({
     },
   },
 });
-
-
 
 
